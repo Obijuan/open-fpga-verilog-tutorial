f400b21f4f1cd594df6b2b2f7d5f6c0ab6902ea0
obijuan <juan@iearobotics.com>
2015-08-16 11:20:56 +0200

    T07: Initial commit

---
+++ tutorial/T07-contador-prescaler/counter4.v
@@ -0,0 +1,23 @@
+module counter4(input clk, output [3:0] data);
+wire clk;
+reg [3:0] data;
+
+//-- Parametro para el prescaler
+parameter N = 22;
+
+//-- Reloj del prescaler
+wire clk_pres;
+
+//-- Instanciar el prescaler
+prescaler #(.N(N))
+  pres1 (
+    .clk_in(clk),
+    .clk_out(clk_pres)
+  );
+
+//-- Incrementar el contador en cada flanco de subida
+always @(posedge(clk_pres)) begin
+  data <= data + 1;
+end
+
+endmodule
