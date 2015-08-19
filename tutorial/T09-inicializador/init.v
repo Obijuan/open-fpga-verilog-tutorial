30f7b38347aa7e71860092fdc0984474051660cd
obijuan <juan@iearobotics.com>
2015-08-19 09:08:20 +0200

    T09: comments added to init.v

--- tutorial/T09-inicializador/init.v
+++ tutorial/T09-inicializador/init.v
@@ -1,4 +1,31 @@
+//----------------------------------------------------------------
+//-- Inicializador
+//-- (c) BQ. August 2015. Written by Juan Gonzalez (obijuan)
+//-- GPL license
+//----------------------------------------------------------------
+//-- Generacion de una señal escalo (0 -> 1) para inicializar
+//-- circuitos digitales
+//----------------------------------------------------------------
+
+
+//-- Version optimizada
+//-- Entrada: Señal de reloj
+//-- Salida: Señal escalón de inicialización
+module init(input wire clk, output ini);
+
+//-- Inicializar la salida a 0 (se pone para que funcione en simulación)
+//-- En síntesis siempre estará a cero con independencia del valor que pongamos
+reg ini = 0;
+  
+//-- En cada flanco de subida se saca un "1" por la salida
+always @(posedge(clk))
+  ini <= 1;
+
+endmodule
+
+
 /*
+//-- Implementacion natural
 module init(input wire clk, output wire ini);
 
 wire din;
@@ -16,14 +43,3 @@ assign ini = dout;
 
 endmodule
 */
-
-module init(input wire clk, output ini);
-reg ini = 0;
-  
-always @(posedge(clk))
-  ini <= 1;
-
-endmodule
-
-
-
