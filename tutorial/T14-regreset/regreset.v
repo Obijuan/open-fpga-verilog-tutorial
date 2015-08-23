9c620f21ce7b42f280a506e428f878ba40199aec
obijuan <juan@iearobotics.com>
2015-08-23 19:13:52 +0200

    T14: comments added

--- tutorial/T14-regreset/regreset.v
+++ tutorial/T14-regreset/regreset.v
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-//-- Secuenciador de 2 estados a partir de registro de 4 bits con
+//-- Secuenciador de 2 estados a partir de dos registros de 4 bits con
 //-- inicialización de carga
 //-- (C) BQ. August 2015. Written by Juan Gonzalez (obijuan)
 //-----------------------------------------------------------------------------
@@ -10,13 +10,15 @@ module regreset(input wire clk, output wire [3:0] data);
 
 //-- Parametros del secuenciador:
 parameter NP = 23;        //-- Bits del prescaler
-parameter INI0 = 4'b1001; //-- Valor inicial a cargar en registro
+parameter INI0 = 4'b1001; //-- Valor inicial para el registro 0
+parameter INI1 = 4'b0111; //-- Valor inicial para el registro 1
 
 //-- Reloj a la salida del presacaler
 wire clk_pres;
 
-//-- Salida del regitro
-wire [3:0] dout;
+//-- Salida de los regitros
+wire [3:0] dout0;
+wire [3:0] dout1;
 
 //-- Señal de inicializacion del reset
 reg rst = 0;
@@ -25,17 +27,26 @@ reg rst = 0;
 always @(posedge(clk_pres))
   rst <= 1;
 
-//-- Registro
+//-- Registro 0
 register #(.INI(INI0))
   REG0 (
     .clk(clk_pres),
     .rst(rst),
-    .din(~dout),
-    .dout(dout)
+    .din(dout1),
+    .dout(dout0)
   );
 
-//-- Conectar el registro con la salida
-assign data = dout;
+//-- Registro 1
+register #(.INI(INI1))
+  REG1 (
+    .clk(clk_pres),
+    .rst(rst),
+    .din(dout0),
+    .dout(dout1)
+  );
+
+//-- Sacar la salida del registro 0 por la del componente
+assign data = dout0;
 
 //-- Prescaler
 prescaler #(.N(NP))
