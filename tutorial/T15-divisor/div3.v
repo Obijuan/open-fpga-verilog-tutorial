41706d183587e05cfe67d64b2245a92878ceb0c1
obijuan <juan@iearobotics.com>
2015-08-24 20:42:05 +0200

    T15: comments added

--- tutorial/T15-divisor/div3.v
+++ tutorial/T15-divisor/div3.v
@@ -1,3 +1,12 @@
+//-----------------------------------------------------------------------------
+//-- Divisor de frecuencia entre 3
+//-- (c) BQ. August 2015. written by Juan Gonzalez (obijuan)
+//-----------------------------------------------------------------------------
+//-- GPL license
+//-----------------------------------------------------------------------------
+
+//-- Entrada: clk_in. Señal original
+//-- Salida: clk_out. Señal de frecuencia 1/3 de la original
 module div3(input wire clk_in, output wire clk_out);
 
 reg [1:0] divcounter = 0;
