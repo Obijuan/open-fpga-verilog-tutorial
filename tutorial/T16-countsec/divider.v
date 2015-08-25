db2a1fb88ebf6a6b6535eebc72435ee032d1119e
obijuan <juan@iearobotics.com>
2015-08-25 19:18:23 +0200

    T16: new file divider.vh with some constants

--- tutorial/T16-countsec/divider.v
+++ tutorial/T16-countsec/divider.v
@@ -5,14 +5,15 @@
 //-- GPL license
 //-----------------------------------------------------------------------------
 
+`include "divider.vh"
+
 //-- Entrada: clk_in. Señal original
 //-- Salida: clk_out. Señal de frecuencia 1/M de la original
 module divider(input wire clk_in, output wire clk_out);
 
 //-- Valor por defecto del divisor
-//-- Como en la iCEstick el reloj es de 12MHz, ponermos un valor de 12M
-//-- para obtener una frecuencia de salida de 1Hz
-parameter M = 12_000_000;
+//-- Lo ponemos a 1 Hz
+parameter M = `F_1Hz;
 
 //-- Numero de bits para almacenar el divisor
 //-- Se calculan con la funcion de verilog $clog2, que nos devuelve el 
