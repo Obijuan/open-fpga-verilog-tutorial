7ff9578df4753c02d1e5f710a476b8eecdc075fa
obijuan <juan@iearobotics.com>
2015-10-24 10:37:42 +0200

    T28: Uart transmitter added. Only the first character is sent by the moment

--- tutorial/T28-ram/buffer.v
+++ tutorial/T28-ram/buffer.v
@@ -7,26 +7,43 @@
 //----------------------------------------------------------------------------
 `default_nettype none
 
+`include "baudgen.vh"
+
 module buffer (input wire clk,
+               input wire rstn,
                input wire rx,
                output wire tx,
                output wire [4:0] leds);
 
+//-- Velocidad de transmision
+parameter BAUD = `B115200;
+
 //-- Fichero con la rom
 parameter ROMFILE = "bufferini.list";
 
-//-- Numero de bits de la direccione
-parameter AW = 6;
+//-- Numero de bits de la direccion
+parameter AW = 4;
 parameter DW = 8;
 
 //-- Cable para direccionar la memoria
-wire [AW-1: 0] addr;
+reg [AW-1: 0] addr;
 wire [DW-1: 0] data_in;
 wire [DW-1: 0] data_out;
-wire rw;
+reg rw;
 
-reg rstn = 0;
-wire clk_delay;
+wire ready;
+reg transmit;
+
+reg rstn_r;
+
+//-- Registrar el reset
+always @(posedge clk)
+  rstn_r <= rstn;
+
+//-------------------- Microordenes
+//-- counter enable: Se pone a 1 cuando haya que acceder a la siguiente
+//-- posicion de memoria
+reg cena;
 
 //-- Instanciar la memoria rom
 genram
@@ -41,10 +58,89 @@ genram
         .rw(rw)
       );
 
+//-- Contador
+always @(posedge clk)
+  if (rstn_r == 0)
+    addr <= 0;
+  else if (cena)
+    addr <= addr + 1;
 
-//-- Inicializador
-always @(negedge clk)
-  rstn <= 1;
+//-- Conectar los leds
+assign leds = data_out[4:0];
+
+//-------- TRANSMISOR SERIE
+//-- Instanciar la Unidad de transmision
+uart_tx #(.BAUD(BAUD))
+  TX0 (
+    .clk(clk),
+    .rstn(rstn),
+    .data(data_out),
+    .start(transmit),
+    .ready(ready),
+    .tx(tx)
+  );
+
+
+//------------------- CONTROLADOR
+localparam READ_1 = 0;  //-- Lectura en memoria
+localparam TRANS_1 = 1;   //-- Comienzo de transmision de caracter
+localparam TRANS_2 = 2;   //-- Esperar a que transmision se estabilice
+
+reg [1: 0] state;
+
+always @(posedge clk)
+  if (rstn_r == 0)
+    state <= READ_1;
+  else
+    case (state)
+      READ_1:
+        //-- Si transmisor listo, pasar al estado de transmitir
+        if (ready) 
+          state <= TRANS_1;
+        else
+          state <= READ_1;
+
+      TRANS_1: state <= TRANS_2;
+      TRANS_2: 
+        //-- Esperar a que ready se ponga a 0
+        if (ready)
+          state <= TRANS_2;
+        else
+          state <= TRANS_2;
+
+      default: state <= READ_1;
+    endcase
+
+always @*
+  case (state)
+    READ_1: begin
+      rw <= 1;
+      cena <= 0;
+      transmit <= 0;
+    end
+
+    TRANS_1: begin
+      rw <= 1;
+      cena <= 1;
+      transmit <= 1;
+    end
+
+    TRANS_2: begin
+      rw <= 1;
+      cena <= 0;
+      transmit <= 0;
+    end
+
+    default: begin
+      rw <= 1;
+      cena <= 0;
+      transmit <= 0;
+    end
+  endcase
+
 
 endmodule
+
+
+
 
--- tutorial/T28-ram/buffer_tb.v
+++ tutorial/T28-ram/buffer_tb.v
@@ -11,21 +11,40 @@ module buffer_tb();
 //-- Para la simulacion se usa un retraso de 2 ciclos de reloj
 parameter ROMFILE = "bufferini.list";
 
+//-- Baudios con los que realizar la simulacion
+localparam BAUD = `B115200;
+localparam DELAY = 10000;
+
+//-- Tics de reloj para envio de datos a esa velocidad
+//-- Se multiplica por 2 porque el periodo del reloj es de 2 unidades
+localparam BITRATE = (BAUD << 1);
+
+//-- Tics necesarios para enviar una trama serie completa, mas un bit adicional
+localparam FRAME = (BITRATE * 11);
+
+//-- Tiempo entre dos bits enviados
+localparam FRAME_WAIT = (BITRATE * 4);
+
+
 //-- Registro para generar la señal de reloj
 reg clk = 0;
 wire tx;
 wire rx;
+reg rstn = 0;
 
 //-- Datos de salida del componente
 wire [4:0] leds;
 
 //-- Instanciar el componente
-buffer #(.ROMFILE(ROMFILE))
+buffer #(.ROMFILE(ROMFILE), .BAUD(BAUD))
   dut(
     .clk(clk),
+    .rstn(rstn),
     .tx(tx),
-    .rx(rx)
+    .rx(rx),
+    .leds(leds)
   );
+
 
 //-- Generador de reloj. Periodo 2 unidades
 always #1 clk = ~clk;
@@ -38,7 +57,10 @@ initial begin
   $dumpfile("buffer_tb.vcd");
   $dumpvars(0, buffer_tb);
 
-  # 140 $display("FIN de la simulacion");
+  # 20  rstn <= 1;
+
+
+   #(FRAME * 20) $display("FIN de la simulacion");
   $finish;
 end
 
