5524686caa38d4a586cbcc91d1da7d3ae8bd56f4
obijuan <juan@iearobotics.com>
2015-10-31 08:45:22 +0100

    T28: genram: comments added

--- tutorial/T28-ram/genram.v
+++ tutorial/T28-ram/genram.v
@@ -9,45 +9,45 @@
 //--  * DW: Numero de bits de los datos
 //--  * ROMFILE: Fichero a usar para cargar la memoria
 //--
-//-- Con este componente podemos hacer memorias rom de cualquier tamaño
+//-- Con este componente podemos hacer memorias ram de cualquier tamaño
 //----------------------------------------------------------------------------
 
 module genram #(             //-- Parametros
          parameter AW = 5,   //-- Bits de las direcciones (Adress width)
          parameter DW = 4)   //-- Bits de los datos (Data witdh)
 
-       (                               //-- Puertos
-         input clk,                    //-- Señal de reloj global
-         input wire [AW-1: 0] addr,    //-- Direcciones
-         input wire rw,                //-- Modo lectura (1) o escritura (0)
-         input wire [DW-1: 0] data_in, //-- Dato de entrada
-         output reg [DW-1: 0] data_out);
+       (        //-- Puertos
+         input clk,                      //-- Señal de reloj global
+         input wire [AW-1: 0] addr,      //-- Direcciones
+         input wire rw,                  //-- Modo lectura (1) o escritura (0)
+         input wire [DW-1: 0] data_in,   //-- Dato de entrada
+         output reg [DW-1: 0] data_out); //-- Dato a escribir
 
-//-- Parametro: Nombre del fichero con el contenido de la ROM
+//-- Parametro: Nombre del fichero con el contenido de la RAM
 parameter ROMFILE = "bufferini.list";
 
 //-- Calcular el numero de posiciones totales de memoria
 localparam NPOS = 2 ** AW;
 
   //-- Memoria
-  reg [DW-1: 0] rom [0: NPOS-1];
+  reg [DW-1: 0] ram [0: NPOS-1];
 
   //-- Lectura de la memoria
   always @(posedge clk) begin
     if (rw == 1)
-    data_out <= rom[addr];
+    data_out <= ram[addr];
   end
 
   //-- Escritura en la memoria
   always @(posedge clk) begin
     if (rw == 0)
-     rom[addr] <= data_in;
+     ram[addr] <= data_in;
   end
 
 //-- Cargar en la memoria el fichero ROMFILE
 //-- Los valores deben estan dados en hexadecimal
 initial begin
-  $readmemh(ROMFILE, rom);
+  $readmemh(ROMFILE, ram);
 end
 
 
